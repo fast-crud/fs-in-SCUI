361922029fb8b7f9bacff678d2b71479e0e18be8
sc <shencheng@heronshn.com>
2022-02-28 09:45:40 +0800

    scTableSelect 增加change事件

--- src/views/vab/tableselect.vue
+++ src/views/vab/tableselect.vue
@@ -11,14 +11,14 @@
 	<el-main>
 		<el-alert title="select深度改造的表格选择器, 非常适用于大量数据选择的场景" type="success" style="margin-bottom:20px;"></el-alert>
 		<el-card shadow="never" header="单选">
-			<sc-table-select v-model="value2" :apiObj="apiObj" :params="params" :table-width="600" :props="props">
+			<sc-table-select v-model="value2" :apiObj="apiObj" :params="params" :table-width="600" :props="props" @change="change">
 				<el-table-column prop="id" label="ID" width="180"></el-table-column>
 				<el-table-column prop="user" label="姓名"></el-table-column>
 			</sc-table-select>
 		</el-card>
 		<div style="height:15px"></div>
 		<el-card shadow="never" header="多选">
-			<sc-table-select v-model="value" :apiObj="apiObj" :table-width="700" multiple :props="props">
+			<sc-table-select v-model="value" :apiObj="apiObj" :table-width="700" multiple :props="props" @change="change">
 				<template #header="{form, submit}">
 					<el-form :inline="true" :model="form">
 						<el-form-item>
@@ -82,7 +82,11 @@
 
 		},
 		methods: {
-
+			//值变化
+			change(val){
+				this.$message('change事件，返回详情查看控制台')
+				console.log(val)
+			}
 		}
 	}
 </script>
